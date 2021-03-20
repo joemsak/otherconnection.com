ac66f98dc53a0c1f56fc219665038d8259b1338c
Joe Sak <joe@joesak.com>
2021-03-20 00:43:10 -0600

    Exclude airbrake in specs

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,8 @@
-require 'airbrake-ruby'
+unless Rails.env.test?
+  require 'airbrake-ruby'
 
-Airbrake.configure do |c|
-  c.project_id = Rails.application.credentials.airbrake[:project_id]
-  c.project_key = Rails.application.credentials.airbrake[:project_key]
+  Airbrake.configure do |c|
+    c.project_id = Rails.application.credentials.airbrake[:project_id]
+    c.project_key = Rails.application.credentials.airbrake[:project_key]
+  end
 end
