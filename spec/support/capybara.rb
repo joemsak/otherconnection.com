b16fb9029c3d28bc80a009915d7c1ea1e74d0d87
Joe Sak <joe@joesak.com>
2021-03-20 12:09:03 -0600

    Set heroku chrome for HEROKU_CI

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,4 +1,22 @@
 require 'capybara/rails'
+require 'webdrivers'
+
+chrome_bin = ENV['GOOGLE_CHROME_SHIM'] if ENV['GOOGLE_CHROME_SHIM'].present?
+
+chrome_capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+  chromeOptions: {
+    args: %w[headless disable-gpu window-size=1400x1400],
+    binary: chrome_bin
+  }
+)
+
+Capybara.register_driver :heroku_chrome do |app|
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: chrome_capabilities
+  )
+end
 
 Capybara.server = :puma, { Silent: true }
 
@@ -21,6 +39,11 @@ RSpec.configure do |config|
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :chrome_headless
+    if ENV["HEROKU_CI"]
+      driven_by :heroku_chrome
+    else
+      driven_by :chrome_headless
+    end
   end
 end
+
