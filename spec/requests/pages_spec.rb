e16cf797ebd4f3deb3efb4f7fa664cdf031f94b8
Joe Sak <joe@joesak.com>
2021-03-31 23:10:37 -0600

    Create page for pages request spec

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe "Pages", type: :request do
   describe "GET /show" do
     it "returns http success" do
+      create(:page, title: "Home")
       get page_path(:home)
       expect(response).to have_http_status(:success)
     end
