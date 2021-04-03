ca1403afc203d355d13590eb956acffa445c4682
Joe Sak <joe@joesak.com>
2021-04-03 11:33:39 -0600

    Fix the record_id to uuid migration to preserve the index and null option

--- db/migrate/20210401033654_change_record_id_to_uuid_on_active_storage_attachments.rb
+++ db/migrate/20210401033654_change_record_id_to_uuid_on_active_storage_attachments.rb
@@ -1,7 +1,13 @@
 class ChangeRecordIdToUuidOnActiveStorageAttachments < ActiveRecord::Migration[6.1]
   def up
     remove_column :active_storage_attachments, :record_id, :integer
-    add_column :active_storage_attachments, :record_id, :uuid
+
+    add_column :active_storage_attachments, :record_id, :uuid, null: false
+
+    add_index :active_storage_attachments,
+      %i[record_type record_id name blob_id],
+      name: "index_active_storage_attachments_uniqueness",
+      unique: true
   end
 
   def down
